ef46f66c566e1b78adb6874082569972f0f425e3
Rajesh Panda <rajesh.situ@gmail.com>
2018-02-15 15:09:21 +0530

    album app changes updated with single screen

---
+++ albums/src/components/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+  const { containerStyle } = styles;
+  return (
+    <View style={containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 2,
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    margin: 5
+  }
+};
+
+export default Card;
---
+++ albums/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = () => {
+  const { viewStyle, textStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    height: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5
+  },
+  textStyle: {
+    fontSize: 20,
+    fontWeight: '600'
+  }
+};
+
+export default Header;
