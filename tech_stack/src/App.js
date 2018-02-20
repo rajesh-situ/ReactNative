4694fb67e2946ed7ef754e7420041f6731c2cc04
Rajesh Panda <rajesh.situ@gmail.com>
2018-02-20 23:43:55 +0530

    added action creator

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -4,12 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <Header headerText="Tech Stack"/>
+        <View style={{ flex: 1 }}>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
       </Provider>
     );
   }
